8a9f868d42916c613cf0ef38b0d0681efaabc19d
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-23 12:55:26 +0530

    js

---
+++ Class11- (Intro to Javascript)/concat.js
@@ -0,0 +1,13 @@
+console.log("10" - 2); // 8
+console.log("10" * "2"); // 20
+console.log("10" / "2"); // 5
+
+ console.log("apples" + 45); //apples45
+ console.log("apples" - 45); //NaN -> Not A Number
+ let res = "apples" * 45; //NaN
+ console.log(res)  // NaN
+ console.log(typeof res)  // number
+
+ console.log("abc" - "bc") //NaN
+
+
--- Class11- (Intro to Javascript)/script.js
+++ Class11- (Intro to Javascript)/script.js
@@ -4,35 +4,40 @@ console.log('hello from VSCode')
 console.log(100+200)
 
 //variables
-var name = 'Priyanshi'
-console.log(name)
+var userName = 'Priyanshi'
+console.log(userName)
+console.log(typeof userName) // String
 
 var age = 65
 console.log(age)
+console.log(typeof age) // number
 
 var age = 25
 console.log(age)
 
 
-let num = 20;
-console.log(num)
+let isPresent = true;
+console.log(isPresent)
+console.log(typeof isPresent) //boolean
 
 // let num = 100 // Error: Identifier 'num' has already been declared
 // console.log(num)
-
 
 const pi = 3.14;
 console.log(pi)
 // const pi = 6.3 // Redeclaration : not allowed
 // pi = 3.1415 // Reassignment : not allowed 
-console.log(pi)
-
+console.log(typeof pi) // number
 
 let psp;
+console.log(typeof psp) //undefined
+
 // const g; // Error: Missing initializer in const declaration 
 
 const g = 9.8;
 psp = 65.34 
 
-
+let nothing = null;
+console.log(nothing) 
+console.log(typeof nothing) // typeof null : object
 
