82312d8fa65596f34b86aad85427b622379167af
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-23 11:35:19 +0530

    variables

---
+++ Class11- (Intro to Javascript)/script.js
@@ -0,0 +1,38 @@
+console.log('hello from VSCode')
+
+// print sum fo 100 and 200
+console.log(100+200)
+
+//variables
+var name = 'Priyanshi'
+console.log(name)
+
+var age = 65
+console.log(age)
+
+var age = 25
+console.log(age)
+
+
+let num = 20;
+console.log(num)
+
+// let num = 100 // Error: Identifier 'num' has already been declared
+// console.log(num)
+
+
+const pi = 3.14;
+console.log(pi)
+// const pi = 6.3 // Redeclaration : not allowed
+// pi = 3.1415 // Reassignment : not allowed 
+console.log(pi)
+
+
+let psp;
+// const g; // Error: Missing initializer in const declaration 
+
+const g = 9.8;
+psp = 65.34 
+
+
+
