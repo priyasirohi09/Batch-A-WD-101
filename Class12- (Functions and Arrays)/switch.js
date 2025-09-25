79bf42167f506402eda61b0b30969e8025751515
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-25 11:48:11 +0530

    break

--- Class12- (Functions and Arrays)/switch.js
+++ Class12- (Functions and Arrays)/switch.js
@@ -1,25 +1,29 @@
 // function that prints fruit color
 
 function checkFruit(fruitName) {
-
-  switch (fruitName) {
+  let fruit= fruitName.toLowerCase()
+  switch (fruit) {
     case "apple":
       console.log('Red fruit')
       break;
     case "orange":
       console.log('orange fruit')
-      break;
+      // break;
     case "grapes":
       console.log('black fruit')
-      break;
+      // break;
     case "mango":
       console.log('yellow fruit')
-      break;
+      // break;
     default:
       console.log('New fruit')
   }
 
 }
 
-checkFruit('apple');
-checkFruit('mango');
+checkFruit('orange');
+// checkFruit('mango');
+// checkFruit('peaches')
+
+// checkFruit('Apple');
+// checkFruit('APPLE');
