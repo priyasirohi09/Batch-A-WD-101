c1f1f1c854ed026800b0190cc49e56deddac65dd
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-30 11:10:08 +0530

    methods

---
+++ Class13- (Arrays & Objects)/array.js
@@ -0,0 +1,9 @@
+let arr = [10, 'Priyanshi', true, 25, -9.5, 25]
+
+// console.log(arr.indexOf(25)); // 3
+// console.log(arr.indexOf(250)); // -1
+// console.log(arr.indexOf(true)); // 2
+// console.log(arr.indexOf(false)); // -1
+
+console.log(arr.includes('Priyanshi')); // true
+console.log(arr.includes('priyanshi')); // false
