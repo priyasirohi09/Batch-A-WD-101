06371e01b02409dda0ecf29308a4bf971291ed80
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-04 10:58:01 +0530

    dot

--- Class14- (execution Context)/object.js
+++ Class14- (execution Context)/object.js
@@ -12,3 +12,15 @@ console.log(person.age);
 
 // Bracket Notation 
 console.log(person['name'])
+
+// Modify Values 
+person.age = 20
+console.log(person);
+
+// Adding a new key-value pair
+person.hobby = 'Sleeping'
+console.log(person);
+
+delete person.isStudent; 
+console.log(person);
+
