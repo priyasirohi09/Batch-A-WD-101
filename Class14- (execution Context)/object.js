5b751720641b3fa388a5ccbf1e800b7a6b7cf0da
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-04 10:50:37 +0530

    object

---
+++ Class14- (execution Context)/object.js
@@ -0,0 +1,14 @@
+let person = {
+  name : "Ridhima" , 
+  age  :  19,
+  isStudent : true , 
+  marks : [80, 90, 100]
+}
+
+console.log(person);
+
+// Dot Notation 
+console.log(person.age);
+
+// Bracket Notation 
+console.log(person['name'])
