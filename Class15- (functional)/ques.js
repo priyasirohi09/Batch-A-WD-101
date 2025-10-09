a01e41f72058c7b22c50dfe53ccfc9869a7da112
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-09 10:58:38 +0530

    ques

---
+++ Class15- (functional)/ques.js
@@ -0,0 +1,6 @@
+// Given an array of radius of diff circle, 
+//  write function that return area of each Circle 
+//  write function that return circumference of each Circle 
+//  write function that return diameter of each Circle 
+
+let radiusArr = [2, 4, 1, 10, 20]
