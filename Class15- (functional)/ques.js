c191d4c657ca175f1dd93d43317591ebc2fe0a91
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-09 11:25:11 +0530

    push

--- Class15- (functional)/ques.js
+++ Class15- (functional)/ques.js
@@ -5,6 +5,27 @@
 
 let radiusArr = [2, 4, 1, 10, 20]
 
+function circleArea(r){
+  return 3.14*r*r;
+}
+function circleCircumference(r){
+  return 2*3.14*r
+}
+function circleDiameter(r){
+  return 2*r
+}
+
+function circleDetail(arr, cb){
+  let ans = []
+  for(let i = 0; i < arr.length; i++){
+    ans.push(cb(arr[i]))
+  }
+  return ans 
+}
+console.log(circleDetail(radiusArr, circleArea))
+console.log(circleDetail(radiusArr, circleCircumference))
+console.log(circleDetail(radiusArr, circleDiameter))
+/*
 function calculateArea(arr){
   let area = []
   for(let i = 0; i < arr.length; i++){
@@ -26,6 +47,8 @@ function calculateDiameter(arr){
   }
   return diameter
 }
+
 console.log(calculateArea(radiusArr))
 console.log(calculateCircumference(radiusArr))
 console.log(calculateDiameter(radiusArr))
+*/
